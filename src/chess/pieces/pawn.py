44a061d1d5d83ce12931437fc078c0da1d900675
Theo GAAD <theo.gaad@gmail.com>
2026-09-01 10:12:29 +0200

    Correction de la gestion de has_moved dans pawn.py.
    Ajout de tests pour vérifier la gestion de has_moved dans test_pawn.py.

--- src/chess/pieces/pawn.py
+++ src/chess/pieces/pawn.py
@@ -10,7 +10,7 @@ class Pawn(Piece):
         list_of_possible_moves: list[tuple[int, int]] = []
         coeff: int = 1 if self.piece_color == PieceColor.WHITE else -1
         offsets: list[tuple[int, int]] = [(1 * coeff, 0), (1 * coeff, 1), (1 * coeff, -1)]
-        if self.has_moved:
+        if not self.has_moved:
             offsets.append((2 * coeff, 0))
         for offset in offsets:
             if 0 <= line + offset[0] < BOARD_SIZE and 0 <= column + offset[1] < BOARD_SIZE:
--- tests/pieces/test_pawn.py
+++ tests/pieces/test_pawn.py
@@ -22,17 +22,36 @@ def test_pawn_wrong_creation(piece_color):
         Pawn(piece_color)
 
 @pytest.mark.parametrize("start, expected_end_position", [
-    ((0, 0), [(1, 0), (2, 0), (1, 1)]), 
-    ((4, 4), [(5, 4), (6, 4), (5, 5), (5, 3)]), 
-    ((3, 7), [(4, 7), (5, 7), (4, 6)]),
+    ((0, 0), [(1, 0), (1, 1), (2, 0)]), 
+    ((4, 4), [(5, 4), (5, 5), (5, 3), (6, 4)]), 
+    ((3, 7), [(4, 7), (4, 6), (5, 7)]),
     ((7, 7), [])])
 def test_white_pawn_get_moves(white_pawn: Pawn, start: tuple[int, int], expected_end_position: list[tuple[int, int]]):
     assert white_pawn.get_moves(start[0], start[1]) == expected_end_position
 
 @pytest.mark.parametrize("start, expected_end_position", [
     ((0, 0), []), 
-    ((4, 4), [(3, 4), (2, 4), (3, 5), (3, 3)]), 
-    ((3, 7), [(2, 7), (1, 7), (2, 6)]),
-    ((7, 7), [(6, 7), (5, 7), (6, 6)])])
+    ((4, 4), [(3, 4), (3, 5), (3, 3), (2, 4)]), 
+    ((3, 7), [(2, 7), (2, 6), (1, 7)]),
+    ((7, 7), [(6, 7), (6, 6), (5, 7)])])
 def test_black_pawn_get_moves(black_pawn: Pawn, start: tuple[int, int], expected_end_position: list[tuple[int, int]]):
+    assert black_pawn.get_moves(start[0], start[1]) == expected_end_position
+
+@pytest.mark.parametrize("start, expected_end_position", [
+    ((0, 0), [(1, 0), (1, 1)]), 
+    ((4, 4), [(5, 4), (5, 5), (5, 3)]), 
+    ((3, 7), [(4, 7), (4, 6)]),
+    ((7, 7), [])
+])
+def test_white_pawn_has_moved_get_moves(white_pawn: Pawn, start: tuple[int, int], expected_end_position: list[tuple[int, int]]):
+    white_pawn.has_moved = True
+    assert white_pawn.get_moves(start[0], start[1]) == expected_end_position
+
+@pytest.mark.parametrize("start, expected_end_position", [
+    ((0, 0), []), 
+    ((4, 4), [(3, 4), (3, 5), (3, 3)]), 
+    ((3, 7), [(2, 7), (2, 6)]),
+    ((7, 7), [(6, 7), (6, 6)])])
+def test_black_pawn_has_moved_get_moves(black_pawn: Pawn, start: tuple[int, int], expected_end_position: list[tuple[int, int]]):
+    black_pawn.has_moved = True
     assert black_pawn.get_moves(start[0], start[1]) == expected_end_position
