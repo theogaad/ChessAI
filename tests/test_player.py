eca35057647624d11f4855eae10e119eef629a18
Theo GAAD <theo.gaad@gmail.com>
2026-09-01 16:20:09 +0200

    Ajout tests
    Ajout des tests de la création d'un objet Player dans test_player.py.

---
+++ tests/test_player.py
@@ -0,0 +1,18 @@
+from src.chess.player import Player
+from src.chess.pieces.piece import PieceColor
+from src.chess.profile import Profile
+import pytest
+
+@pytest.fixture
+def player_white() -> Player:
+    return Player(Profile(), PieceColor.WHITE)
+
+@pytest.fixture
+def player_black() -> Player:
+    return Player(Profile(), PieceColor.BLACK)
+
+def test_player_white(player_white: Player):
+    assert player_white.color == PieceColor.WHITE
+
+def test_player_black(player_black: Player):
+    assert player_black.color == PieceColor.BLACK
