f202e0be4d0126172f51a00a1b4cb465a9887065
Theo GAAD <theo.gaad@gmail.com>
2026-09-15 12:28:32 +0200

    Ajout tests
    Dans test_game.py :
    Ajout du test test_game_get_color_every_legal_moves
    Ajout du test test_game_promotion
    Ajout du test test_game_play_move
    Ajout du test test_game_play_move_illegal_move
    Ajout du test test_game_play_move_wrong_color

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,10 +1,11 @@
 from src.chess.case import Case
+from src.chess.exceptions.illegal_move_error import IllegalMoveError
 from src.chess.game import Game
 from src.chess.board import Board
-from src.chess.move import Move
+from src.chess.move import Move, SpecialMove
 from src.chess.player import Player
 from src.chess.profile import Profile
-from src.chess.pieces.piece import PieceColor
+from src.chess.pieces.piece import Piece, PieceColor
 from src.chess.pieces.bishop import Bishop
 from src.chess.pieces.king import King
 from src.chess.pieces.pawn import Pawn
@@ -65,6 +66,27 @@ def test_game_get_legal_moves(game_with_empty_board: Game, line: int, column: in
     game_with_empty_board.board.grid[7][4].content = Rook(PieceColor.BLACK)
 
     assert game_with_empty_board.get_legal_moves(line, column) == [game_with_empty_board.board.grid[move[0]][move[1]] for move in expected_legal_moves]
+
+@pytest.mark.parametrize("expected_legal_moves", [
+    (0, 3),
+    (1, 3),
+    (0, 5),
+    (1, 5),
+    (1, 4),
+    (7, 4),
+    (4, 4),
+    (7, 4),
+    (1, 4)
+])
+def test_game_get_color_every_legal_moves(game_with_empty_board: Game, expected_legal_moves: tuple[int, int]) -> None:
+    game_with_empty_board.board.grid[0][4].content = King(PieceColor.WHITE)
+    game_with_empty_board.board.grid[4][7].content = Bishop(PieceColor.WHITE)
+    game_with_empty_board.board.grid[4][1].content = Queen(PieceColor.WHITE)
+    game_with_empty_board.board.grid[7][4].content = Rook(PieceColor.BLACK)
+
+    legal_moves: list[Case] = game_with_empty_board.get_color_every_legal_moves(PieceColor.WHITE)
+    assert len(legal_moves) == 9
+    assert game_with_empty_board.board.grid[expected_legal_moves[0]][expected_legal_moves[1]] in legal_moves
 
 def test_game_switch_players(normal_game: Game) -> None:
     current_player_before_switch: Player = normal_game.current_player
@@ -135,3 +157,51 @@ def test_game_is_game_over(game_with_empty_board: Game) -> None:
     game_with_empty_board.board.grid[7][0].content = Rook(PieceColor.BLACK)
     game_with_empty_board.board.grid[1][0].content = Queen(PieceColor.BLACK)
     assert game_with_empty_board.is_game_over(game_with_empty_board.current_player) == True
+
+def test_game_promotion(game_with_empty_board: Game) -> None:
+    game_with_empty_board.board.grid[1][0].content = Pawn(PieceColor.BLACK)
+    game_with_empty_board.board.grid[6][0].content = Pawn(PieceColor.WHITE)
+    game_with_empty_board.board.grid[6][4].content = King(PieceColor.BLACK)
+    game_with_empty_board.board.grid[1][4].content = King(PieceColor.WHITE)
+
+    game_with_empty_board.play_move(Move(game_with_empty_board.board.grid[6][0], game_with_empty_board.board.grid[7][0], SpecialMove.PROMOTION, Rook))
+    assert isinstance(game_with_empty_board.board.grid[7][0].content, Rook)
+    assert game_with_empty_board.board.grid[7][0].content.piece_color == PieceColor.WHITE
+
+    game_with_empty_board.play_move(Move(game_with_empty_board.board.grid[1][0], game_with_empty_board.board.grid[0][0], SpecialMove.PROMOTION, Queen))
+    assert isinstance(game_with_empty_board.board.grid[0][0].content, Queen)
+    assert game_with_empty_board.board.grid[0][0].content.piece_color == PieceColor.BLACK
+
+@pytest.mark.parametrize("case1, case2", [
+    ((1, 0), (3, 0)),
+    ((6, 0), (4, 0)),
+    ((0, 1), (2, 2)),
+    ((7, 1), (5, 2)),
+])
+def test_game_play_move(normal_game: Game, case1: tuple[int, int], case2: tuple[int, int]) -> None:
+    case: Case = normal_game.board.grid[case1[0]][case1[1]]
+    assert isinstance(case.content, Piece)
+    piece: Piece = case.content
+    if piece.piece_color != normal_game.current_player.color:
+        normal_game.switch_players()
+    assert piece.piece_color == normal_game.current_player.color
+    move: Move = Move(case, normal_game.board.grid[case2[0]][case2[1]])
+    normal_game.play_move(move)
+    assert normal_game.board.grid[case2[0]][case2[1]].content == piece
+    assert case.content is None
+    assert normal_game.moves[-1] == move
+    assert normal_game.current_player.color != piece.piece_color
+
+def test_game_play_move_illegal_move(normal_game: Game) -> None:
+    case: Case = normal_game.board.grid[0][0]
+    assert isinstance(case.content, Piece)
+    move: Move = Move(case, normal_game.board.grid[2][2])
+    with pytest.raises(IllegalMoveError):
+        normal_game.play_move(move)
+
+def test_game_play_move_wrong_color(normal_game: Game) -> None:
+    case: Case = normal_game.board.grid[6][0]
+    assert isinstance(case.content, Piece)
+    move: Move = Move(case, normal_game.board.grid[4][0])
+    with pytest.raises(IllegalMoveError):
+        normal_game.play_move(move)
