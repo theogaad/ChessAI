df73999b36544a87ff0b7306ad1d53d3cfaa4ec1
Theo GAAD <theo.gaad@gmail.com>
2026-09-22 15:03:25 +0200

    Ajout de la méthode init_players dans Game

--- src/chess/game.py
+++ src/chess/game.py
@@ -2,6 +2,7 @@ from random import shuffle
 from src.chess.board import Board
 from src.chess.player import Player
 from src.chess.profile import Profile
+from src.chess.utils import PieceColor, WHITE, BLACK, verify_type
 
 class Game:
     """Représente une partie d'échec."""
@@ -9,4 +10,24 @@ class Game:
         self, 
         profiles: tuple[Profile, Profile]
     ) -> None:
-        self.board = Board()
+        self.players: tuple[Player, Player]
+        self.board: Board = Board()
+
+
+    def init_players(self, profiles: tuple[Profile, Profile]) -> None:
+        """Initialise les joueurs à partir des profils donnés et en leur attribuant une couleur aléatoire.
+        
+        Args:
+            profiles: Tuple de profils auquel les joueurs sont associés.
+            
+        Raises:
+            TypeError: Si ``profiles`` n'est pas un tuple ou si ses éléments ne sont pas des instances de ``Profile``."""
+        verify_type(profiles, tuple, "init_players(profiles)", "profiles")
+
+        for profile in profiles:
+            verify_type(profile, Profile, "init_players(profiles)", "profiles[x]")
+
+        colors: list[PieceColor] = [WHITE, BLACK]
+        shuffle(colors)
+        self.players = (Player(profiles[0], colors[0]), Player(profiles[1], colors[1]))
+        
